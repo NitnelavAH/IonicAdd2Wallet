e95b044d76e25e22e898d6b14f3f7427dae50a34
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2022-03-06 21:13:23 -0600

    build ios config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nitnelav.pass.wallet',
+  appId: 'io.ionic.starter',
   appName: 'IonicPassToWallet',
   webDir: 'www',
   bundledWebRuntime: false
